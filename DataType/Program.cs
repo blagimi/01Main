cd8f2d9819a9ac95e91e9a9038ceb72229ff6894
Александр Могильный <blagimi@outlook.com>
2023-08-04 23:11:28 +0500

    02

---
+++ DataType/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+String name = "Alex";
+int age = 33;
+bool isEmployed = false;
+double weight = 59.5;
+
+Console.WriteLine($"Имя:      {name}");
+Console.WriteLine($"Возраст:  {age}");
+Console.WriteLine($"Вес:      {weight}");
+Console.WriteLine($"Работает: {isEmployed}");
--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,2 +1,6 @@
 ﻿string name = "Alex";
 Console.WriteLine($"Привет {name}");
+name = "Bob";
+Console.WriteLine($"Привет {name}");
+const string NAME2 = "Alex";
+Console.WriteLine($"Привет {NAME2}");
