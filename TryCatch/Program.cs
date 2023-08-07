63fb50966a3addc111d11c27d1fe03ca5434e87c
Александр Могильный <blagimi@outlook.com>
2023-08-07 23:43:03 +0500

    05

---
+++ TryCatch/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+try
+{
+    int a = 33;
+    int b = 600;
+    byte c = checked((byte)(a + b));
+    Console.WriteLine(c);
+}
+catch (OverflowException ex)
+{
+    Console.WriteLine(ex.Message);
+}
