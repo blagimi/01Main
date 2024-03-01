84b45a802694a6a171251304ad4648f827497b99
Александр Могильный <blagimi@outlook.com>
2024-03-01 23:22:37 +0500

    0.0.0

--- HelloApp/Program.cs
+++ HelloApp/Program.cs
@@ -1,4 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.Write("Введите своё имя: ");
 string name = Console.ReadLine();
-Console.WriteLine($"Привет {name}");
+Console.WriteLine($"Привет {name}, завтра начинаешь");
