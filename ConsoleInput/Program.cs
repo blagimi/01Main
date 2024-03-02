99f6901d1f6cd695255e55429bb135475d11435b
Александр Могильный <blagimi@outlook.com>
2024-03-02 23:40:10 +0500

    0.0.1

---
+++ ConsoleInput/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+string hello = "Привет";
+Console.WriteLine(hello);
+string name = "Alex";
+int age = 33;
+double height = 1.87;
+Console.WriteLine($"Имя: {name} Возраст: {age} Рост: {height}m");
