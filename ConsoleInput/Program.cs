3af9f5aaae6b8499dc3144cf437addf7372ae5dd
Александр Могильный <blagimi@outlook.com>
2024-03-03 17:33:38 +0500

    0.0.2

--- ConsoleInput/Program.cs
+++ ConsoleInput/Program.cs
@@ -1,8 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
-string hello = "Привет";
-Console.WriteLine(hello);
-string name = "Alex";
-int age = 33;
-double height = 1.87;
-Console.WriteLine($"Имя: {name} Возраст: {age} Рост: {height}m");
-Console.ReadLine();
+/// <summary>Вывод на экран параметров</summary>
+{
+    string hello = "Привет";
+    Console.WriteLine(hello);
+    string name = "Alex";
+    int age = 33;
+    double height = 1.87;
+    Console.WriteLine($"Имя: {name} Возраст: {age} Рост: {height}m");
+    Console.ReadLine();
+}
---
+++ ЗаменаЗначенийПеременной/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+string name = "Tom";    // Инициализация переменной name и присваеваем ей значение Tom
+Console.WriteLine($"Hello, {name}");    // Результат Hello, Tom
+name = "Bob";           // Заменяем значение переменной на Bob;
+Console.WriteLine($"Hello, {name}");    // Результат Hello, Bob
