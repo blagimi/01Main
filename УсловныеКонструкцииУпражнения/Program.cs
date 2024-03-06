00561f1e6b6d2810fadbf9992317186aa1ae5e83
Alex <blagimi@outlook.com>
2024-03-06 19:14:58 +0500

    0.0.8

--- УсловныеКонструкцииУпражнения/Program.cs
+++ УсловныеКонструкцииУпражнения/Program.cs
@@ -1,7 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System.ComponentModel.Design;
-
-Console.WriteLine("Выберите упражнение от 1 до 7");
+///<summary>Вызов конструкции свитч кейс</summary>
+///<remarks>В зависимости от введеной пользователем цифры выполняет связанную с этим номером инструкцию</remarks>
+Console.WriteLine("Выберите упражнение от 1 до 4");
 int practice = Convert.ToInt32(Console.ReadLine());
 switch (practice)
 {
@@ -21,8 +22,9 @@ switch (practice)
         Console.WriteLine("Неверно выбран номер");
         break;
 }
-
-
+///<summary>Вызов метода сравнениях 2х цифр</summary>
+///<remarks>Проверяет какое из введенных числе больше</remarks>
+///<returns>Вызывает консоль и выводит сообщение на экран</returns>
 void methodOne ()
 {
     Console.WriteLine("1) Сравнение 2х цифр.");
@@ -37,6 +39,9 @@ void methodOne ()
     else
         Console.WriteLine($"Цифра {inputNumber1} и {inputNumber2} равны");
 }
+///<summary>Проверка цифры на попадание в заданный диапозон 2х цифр</summary>
+///<remarks>Ввводимая цифра должна быть в диапозоне от 5 до 10</remarks>
+///<return>Вызывает консоль и выводит сообщение на экран</return>
 void methodTwo()
 {
     Console.WriteLine("2) Проверка диапозона цифр");
@@ -47,7 +52,9 @@ void methodTwo()
     else
         Console.WriteLine("Неизвестная цифра");
 }
-
+///<summary>Проверка цифры на равенство 2м другим цифрам</summary>
+///<remarks>Вводима цифра должна равняться 5 или 10</remarks>
+///<return>Вызывает консолько и выводит сообщение на экран</return>
 void methodThree()
 {
     Console.WriteLine("3) Проверка цифр на равенство");
@@ -58,7 +65,9 @@ void methodThree()
     else
         Console.WriteLine("Неизвестная цифра");
 }
-
+///<summary>Расчёт процентов по сумме на вкладе в банке</summary>
+///<remarks>Вводится сумма вклада после чего в зависимости от суммы вклада к ней добавляется определённый процент + бонус от банка</remarks>
+///<return>Вызывает консоль и выводит итоговую сумму вклада + проценты по вкладу + бонус</return>
 void methodFour()
 {
     Console.WriteLine("4) Расчёт процента по вкладам");
---
+++ Циклы/Program.cs
@@ -0,0 +1,72 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Цикл for
+Console.WriteLine("1) --- ");
+for (int i = 1; i < 4; i++)
+{
+    Console.Write($"{i} ");   // 1 2 3
+}
+Console.WriteLine();
+/* Если блок цикла for содержит одну инструкцию, то мы можем его сократить, убрав фигурные свобки:
+ *for (int i = 1; i < 4; i++)
+ *   Console.WriteLine(i);
+ * или так
+ * for (int i = 1; i < 4; i++) Console.WriteLine(i); */
+// Так же можно определять несколько переменных в обьявлении цикла
+Console.WriteLine("2) --- ");
+for (int i = 1, j = 1; i < 10;i++,j++)
+    Console.Write($"{i*j} ");    // 1 4 9 16 25 36 49 64 81
+// Цикл do..while
+Console.WriteLine();
+Console.WriteLine("3) --- ");
+int a = 6;
+do    // Выполниться хотя бы один раз
+{
+    Console.Write($"{a} ");   // 6 5 4 3 2 1
+    a--;
+}
+// Цикл while
+while (a > 0);
+Console.WriteLine();
+Console.WriteLine("4) --- ");
+int b = 6;
+while (b>0)
+{
+    Console.Write($"{b} ");
+    b--;
+}
+Console.WriteLine();
+// Цикл foreach
+Console.WriteLine("5) --- ");
+foreach (char c in "Tom")
+{
+    Console.Write($"{c} ");
+}
+Console.WriteLine();
+// Оператор break
+Console.WriteLine("6) --- ");
+for (int n = 0; n < 9; n++)
+{
+    if (n == 5)
+        break;  //  Завершает цикл когда итерация доходит до значения 5
+    Console.Write($"{n} ");
+}
+Console.WriteLine();
+// Оператор continue
+Console.WriteLine("7) --- ");
+for (int m = 0; m < 9; m++)
+{
+    if (m == 5) // Пропускает итерацию в которой значение равно 5 и продолжает цикл
+        continue;
+    Console.Write($"{m} ");
+}
+Console.WriteLine();
+// Вложенные циклы
+Console.WriteLine("8) --- ");
+for (int r = 1; r < 10; r++)
+{
+    for (int e = 1; e < 10; e++)
+    {
+        Console.Write($"{r * e} \t");
+    }
+    Console.WriteLine();    // Таблица умножения
+}
