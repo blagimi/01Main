3d22c2c594ca47cafd68b826541ea761af60bbc8
Alex <blagimi@outlook.com>
2024-03-11 23:46:42 +0500

    0.1.2

--- ЗадачиСМассивами/Program.cs
+++ ЗадачиСМассивами/Program.cs
@@ -10,3 +10,20 @@ foreach(int number in numbers)
     }
 }
 Console.WriteLine($"Количество положительных чисел {result}");
+
+// Инверсия массива
+int[] nums = [-4, -3, -2, -1, 0, 1, 2, 3, 4];
+int n = nums.Length;    // Длина массива
+int k = n / 2;          // Середина массива
+int temp;               // Вспомогальный элемент для обмена значениями
+
+for (int i = 0; i < k; i++)
+{
+    temp = nums[i];
+    nums[i] = nums[n - i - 1];
+    nums[n-i-1] = temp;
+}
+foreach (int i in nums)
+{
+    Console.Write($"{i} \t");
+}
