5ca235553f0d06868625b8f9e02693ffc1ebac0f
Alex <blagimi@outlook.com>
2024-03-13 22:02:29 +0500

    0.1.4

---
+++ ВыходныеПараметры/Program.cs
@@ -0,0 +1,74 @@
+﻿// Выходные параметры. Модификатор out
+/* 
+Параметры помимо входных, которые передаются методу. Так же могут быть и выходными. 
+Что бы сделать параметр выходным, перед ним ставится модификатор out
+*/
+
+static void Sum (int x, int y, out int result)
+{
+    // При указании выходного параметров он обязательно должен быть указан в методе
+    result = x + y; 
+}
+int number;
+Sum(10,15, out number);
+System.Console.WriteLine(number);   // 25
+// Возвращение из метода нескольких значений
+/*
+Метод GetRectangleData который получает ширину и высоту прямоугольника (параметры width и height)
+Выходные параметры мы используем для подсчёта площади и периметра прямоугольника
+*/
+static void GetRectangleData(int width, int height, out int rectArea, out int rectPerimetr)
+{
+    rectArea = width * height;          // Площадь прямоугольника - произвидение ширины на высоту
+    rectPerimetr = (width+height)*2;    // Периметр прямоугольника - сумма длин всех сторон
+}
+int area;
+int perimetr;
+GetRectangleData(10,20, out area, out perimetr);
+System.Console.WriteLine($"Площадь прямоугольника: {area}");        //  200
+System.Console.WriteLine($"Периметр прямоугольника: {perimetr}");   // 60
+/*
+При этом можно опеределять переменные которые передаются out-параметрам при вызове метода.
+GetRectangleData(10,20, out int area, out int perimetr);
+Так же если неизвестен тип который будет присвоен параметрам то можно их определить используя var
+GetRectangleData(10,20, out var area, out var perimetr);
+*/
+// Входные параметры.
+// Модификатор In
+static void GetRectangleData2(in int width, in int height, out int rectArea, out int rectPerimetr)
+{
+    //width = 25; Невозможно изменить передаваемоё значение, аргумент передаётся в параметр только для чтения
+    rectArea = width * height;
+    rectPerimetr = (width+height)*2;
+}
+int w = 10;
+int h = 20;
+GetRectangleData2(w,h, out var area2, out var perimetr2);
+System.Console.WriteLine($"Площадь прямоугольника: {area2}");        //  200
+System.Console.WriteLine($"Периметр прямоугольника: {perimetr2}");   // 60
+/*
+Передаа по ссылке в некоторых случаях может увеличить производительность а использование оператора in
+гарантирует что значение переменных которые передаются параметрам нельзя будет изменить в этом методе. 
+*/
+// ref-параметры только для чтения
+/*
+Передача по ссылке имеет возможность изменять значение ref- параметра. Однако иногда это может быть нежелательно.
+Что бы гарантировать что ref-параметр не изменит своего значения можно применять ref-параметр только для чтения.
+Такие параметры описываются ключевым словом readonly
+*/
+void ReadonlyIncrement (ref readonly int n)
+{
+    // n++; Невозможно провести операцию инкремента, параметр только для чтения
+    System.Console.WriteLine($"Число в методе ReadonlyIncrement: {n}");                 //  5
+}
+int readonlyNumber = 5;
+Console.WriteLine($"Число до метода Increment: {readonlyNumber}");                      //  5
+ReadonlyIncrement (ref readonlyNumber);
+System.Console.WriteLine($"Число после метода ReadonlyIncrement: {readonlyNumber}");    //  5
+/*
+В данном случае в метод ReadonlyIncrement параметр n передаётся по ссылке только для чтения.
+При попытке изменить его значение мы получим ошибку. 
+*/
+
+
+Console.ReadKey();
--- ПараметрыМетодов/Program.cs
+++ ПараметрыМетодов/Program.cs
@@ -5,8 +5,56 @@
 *    // действия метода
 *  } 
 */
+using System.Security.AccessControl;
+
 static void PrintMessage (string message)
 {
     Console.WriteLine(message);
 }
 PrintMessage("HelloWorld");
+// Метод складывающий 2 числа
+static void Sum(int x, int y)   // Передача методу 2х чисел в качестве параметров
+{
+    int result = x + y;   // Сложение аргументов 2х параметров
+    System.Console.WriteLine($"{x}+{y} = {result}");    // Вывод на экран аргументы и результат сложения
+}
+Sum(10,15); // 10 + 15 = 25
+
+/* Укороченная запись методов
+*  void Sum (int x, int y) => Console.WriteLine($"{x}+{y}={x+y}") 
+*/
+
+/*
+* Передаваемые параметру значения могут представлять значения переменных
+* или результат работы сложных выражений, которые возвращают значение.
+*/
+int a = 10, b = 15, c = 6;
+Sum (a,b);      // 10 + 15 = 25
+Sum (3,c);      //  3 + 6 = 9
+Sum (14, 4+c);  //  14 + 10 = 24
+
+// При передачи параметров нужно учитывать тип параметров между аргументами и параметрами, они должны быть равны.
+static void PrintPerson (string name, int age)
+{
+    System.Console.WriteLine($"Name: {name} Age: {age}");
+}
+
+PrintPerson ("Tom", 24);    // Name: Tom Age: 24
+
+// Необязательные параметры
+// Параметры значение которые присвоенно по умолчанию и использует их при остуствии передачи аргументов
+static void PrintEmploye (string name, int age = 1, string company = "Undefined")
+{
+    System.Console.WriteLine($"Name: {name} Age: {age} Company: {company}");
+}
+
+PrintEmploye("Tom", 37, "Microsoft");   //  Tom 37  Microsoft
+PrintEmploye("Tom",37);                 //  Tom 37  Undefined
+PrintEmploye("Tom");                    //  Tom 1   Undefined
+
+//  Именованные параметры
+//  Используются для передачи аргументов по имени а не по порядку
+PrintEmploye(age: 41, name: "Bob");                 // Bob 41 Undefined
+PrintEmploye("Tom",company:"Microsoft", age: 37);   // Tom 37 Microsoft
+PrintEmploye(company:"Google", name:"Sam");         // Sam 1  Google
+
