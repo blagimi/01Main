96e2c945b5c6b08ddd7b3a194dae6a9c2616e44d
Alex <blagimi@outlook.com>
2024-03-15 21:57:07 +0500

    0.1.6

---
+++ ЛокальныеФункции/Program.cs
@@ -0,0 +1,51 @@
+﻿// Локальные функции это функции определенные внутри методов.
+// Метод сравнение суммы чисел 2х массивов
+
+void Compare(int[] mass1, int[] mass2)
+{
+    int Sum(int[] tmpMass)
+    {
+        int result = 0;
+        foreach (int n in tmpMass)
+        {
+            result += n;
+        }
+        return result;
+    }
+    int massSum1 = Sum(mass1);
+    int massSum2 = Sum(mass2);
+    if (massSum1>massSum2)
+        System.Console.WriteLine("Массив 1 больше массива 2");
+    else if (massSum2>massSum1)
+        System.Console.WriteLine("Массив 2 больше массива 1");
+    else
+        System.Console.WriteLine("Массивы равны");
+}
+
+int[] massive1 = {1,2,3,4,5};
+int[] massive2 = {1,2,3,4};
+Compare(massive1,massive2);
+
+//Статические локальные функции
+// Не могут обращаться к содержимому метода, для передачи нужно указывать параметры
+
+int Sum (int[] numbers)
+{
+    int result = 0;
+    int limit = 0;
+    foreach(int number in numbers)
+    {
+        if (IsPassed(number,limit)) result += number;   // Сложение положительных чисел
+    }
+    return result;
+
+    static bool IsPassed (int number, int lim)  // Проверка на числа больше 0
+    {
+        //return number > limit;  // Не имеет доступа к limit
+        return number > lim;
+    }
+}
+int[] example = {1,2,3,4,-5,-6,-7,-8};
+Sum(example);
+System.Console.WriteLine(Sum(example));
+Console.ReadKey();
---
+++ РекурсивныеФункции/Program.cs
@@ -0,0 +1,41 @@
+﻿/* Рекурсивные функции.
+Конструкция при которой функция вызывает саму себя.
+При создании рекурсивной функции в ней всегда должен быть базовый вариант
+с которого начинается вычисление функции.
+*/
+// Рекурсивная функция факториала
+// n! = 1*2*3..*n. Для нахождения факториала нужно перемножить все числа до заданого числа.
+static int Factorial (int n)
+{
+    if (n==1) return 1;                 // Базовый вариант
+    return n * Factorial(n-1);          // Рекурсивный спуск
+}
+System.Console.WriteLine(Factorial(4)); // 24
+// Рекурсивная функция Фибоначчи
+// f(n) = f(n-1)+f(n-2) Сумма 2х предыдущих чисел Фибоначчи
+static int Fibonachi(int n)
+{
+    if (n == 0 || n == 1) return n; // fib 0 = 0 fib 1 = 1
+    return Fibonachi(n-1)+Fibonachi(n-2);
+}
+System.Console.WriteLine(Fibonachi(5)); // 5 fib(4)+fib(3)
+/* 
+Рекурсии и циклы.
+Вместо рекурсий можно использовать и циклы они более быстрые чем рекурсия и эффективней.
+*/
+static int Fibonachi2(int n)
+{
+    int result = 0;
+    int b = 1;
+    int temp;
+    for (int i = 0; i < n; i++)
+    {
+        temp = result;
+        result = b;
+        b+=temp;
+    }
+    return result;
+}
+System.Console.WriteLine(Fibonachi2(6));
+
+Console.ReadKey();
