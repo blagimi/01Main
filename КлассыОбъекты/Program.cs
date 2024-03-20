a7386398590495c262e64376e732b28aa4d700a2
Alex <blagimi@outlook.com>
2024-03-20 23:32:03 +0500

    3.1.1

---
+++ КлассыОбъекты/Program.cs
@@ -0,0 +1,18 @@
+﻿Person tom = new Person();
+string personName = tom.name;
+int personAge = tom.age;
+Console.WriteLine($"Имя: {personName}  Возраст {personAge}");
+
+tom.name = "Tom";
+tom.age = 37;
+tom.Print();
+class Person
+{
+    public string name = "Undef";
+    public int age;
+    public void Print()
+    {
+        Console.WriteLine($"Имя: {name} Возраст: {age}");
+    }
+}
+
