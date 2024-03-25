4ff64a4173b8644ddd3be54a8a0e8b2dee218d50
Alex <blagimi@outlook.com>
2024-03-25 23:52:17 +0500

    3.4.1

---
+++ Структуры/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+ * Определение структуры
+ * struct имя_структуры
+ * {
+ *   // элементы структуры
+ * }
+ */
+Person tom = new();
+Person bob = new("Bob");
+Person sam = new("Sam", 25);
+
+tom.Print();    // Имя: Tom  Возраст: 1
+bob.Print();    // Имя: Bob  Возраст: 1 
+sam.Print();    // Имя: Sam  Возраст: 25
+
+struct Person
+{
+    public string name;
+    public int age;
+
+    public Person() : this("Tom")
+    { }
+    public Person(string name) : this(name, 1)
+    { }
+    public Person(string name, int age)
+    {
+        this.name = name;
+        this.age = age;
+    }
+    public void Print() => Console.WriteLine($"Имя: {name}  Возраст: {age}");
+}
