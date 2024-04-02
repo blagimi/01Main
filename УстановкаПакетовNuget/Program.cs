291806ebb716dc5c48fbdb47821e93a39c8e6671
Alex <blagimi@outlook.com>
2024-04-03 01:13:14 +0500

    3.16.1

---
+++ УстановкаПакетовNuget/Program.cs
@@ -0,0 +1,10 @@
+﻿using Newtonsoft.Json;
+
+Person tom = new("Tom", 38);
+string jsonInput = JsonConvert.SerializeObject(tom);
+Console.WriteLine(jsonInput);
+class Person(string name, int age)
+{
+    public string Name { get; set; } = name;
+    public int Age { get; set; } = age;
+}
