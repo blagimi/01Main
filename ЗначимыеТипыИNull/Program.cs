ccceafb2cce52d8080ccf2e6b65c38129d82389e
Alex <blagimi@outlook.com>
2024-04-05 23:53:22 +0500

    3.19.1

---
+++ ЗначимыеТипыИNull/Program.cs
@@ -0,0 +1,16 @@
+﻿PrintNullable(5);       // 5
+PrintNullable(null);    // параметр равен null
+
+void PrintNullable(int? number)
+{
+    if (number.HasValue)
+    {
+        Console.WriteLine(number.Value);
+        // аналогично
+        Console.WriteLine(number);
+    }
+    else
+    {
+        Console.WriteLine("параметр равен null");
+    }
+}
