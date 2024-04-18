a48ef76e2f672e376e12423e56fa297ad11b8359
Alex <blagimi@outlook.com>
2024-04-18 16:51:59 +0500

    4.4.1

---
+++ СкрытиеМетодовИСвойств/Employee.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace СкрытиеМетодовИСвойств
+{
+    internal class Employee : Person
+    {
+        public new string Name
+        {
+            get => $"Mr./Ms. {base.Name}";
+            set => base.Name = value;
+        }
+        public string Company { get; set; }
+        public new readonly static int minAge = 18;
+        public new const string TYPENAME = "Employee";
+        public Employee(string name, string company) : base(name)
+        {
+            Company = company;
+        }
+        /// <summary>
+        /// Скрытие метода Print из базового класса и переопределение его в Employee
+        /// </summary>
+        public new void Print()
+        {
+            Console.WriteLine($"Name: {Name} Company: {Company}");
+        }
+    }
+}
---
+++ СкрытиеМетодовИСвойств/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace СкрытиеМетодовИСвойств
+{
+    internal class Person
+    {
+        public string Name { get; set; }
+        public readonly static int minAge = 1;
+        public const string TYPENAME = "Person";
+        public Person (string name)
+        {
+            Name = name;
+        }
+        /// <summary>
+        /// Метод Print из базового класса Person
+        /// </summary>
+        public void Print()
+        {
+            Console.WriteLine($"Name: {Name}");
+        }
+    }
+}
