ba621f16dd3dde0cb7e06f810330bd879b2ddc0a
Alex <blagimi@outlook.com>
2024-04-18 16:03:30 +0500

    4.3.1

---
+++ ВиртуальныеМетодыИСвойства/Employee.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ВиртуальныеМетодыИСвойства
+{
+    internal class Employee : Person
+    {
+        public string Company { get; set; }
+        public override int Age 
+        {
+            get => base.Age; 
+            set { if (value > 17 && value < 110) base.Age = value; else Console.WriteLine("Недопустимый возраст"); } 
+        }
+        public Employee(string name, int age, string company) : base(name,age)
+        {
+            Company = company;
+            Console.WriteLine("Возраст должен превышать 17 лет");
+            base.Age = 18;
+        }
+        /// <summary>
+        /// Переопределенный метод в классе наследнике из основного класса Person
+        /// </summary>
+        /// <remarks>Выводит имя человека и название компании в которой он работает</remarks>
+        public override sealed void PersonPrint()
+        {
+            Console.WriteLine($"{Name}, {Age} работает в {Company}");
+        }
+    }
+}
---
+++ ВиртуальныеМетодыИСвойства/Person.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Cache;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ВиртуальныеМетодыИСвойства
+{
+    internal class Person 
+    {
+        int age = 1;
+        public string Name { get; set; }
+        public virtual int Age
+        {
+            get => age;
+            set 
+            {
+                if (value > 0 && value < 110) age = value; 
+                else Console.WriteLine("Недопустимый возраст");
+            }
+        } 
+        public Person (string name)
+        {
+            Name = name;
+        }
+        public Person (string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+        /// <summary>
+        /// Метод основного класса Person с возможностью для переопределения в классах наследниках
+        /// </summary>
+        /// <remarks>
+        /// Выводит имя человека
+        /// </remarks>
+        public virtual void PersonPrint()
+        {
+            Console.WriteLine($"{Name},{Age}");
+        }
+    }
+}
