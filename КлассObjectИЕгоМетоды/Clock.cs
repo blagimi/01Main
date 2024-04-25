3ecf4b974f9e84832a547401081915047987c5a2
Alex <blagimi@outlook.com>
2024-04-25 23:55:38 +0500

    4.7.1

---
+++ КлассObjectИЕгоМетоды/Clock.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace КлассObjectИЕгоМетоды
+{
+    internal class Clock
+    {
+        public byte Hours { get; set; }
+        public byte Minutes { get; set; }
+        public byte Seconds { get; set; }
+        /// <summary>
+        /// Переопределение метода из базового класса object
+        /// </summary>
+        /// <returns>
+        /// Выводит указанное время на консоль в виде строки
+        /// </returns>
+        public override string ToString()
+        {
+            return $"{Hours}:{Minutes}:{Seconds}";
+        }
+    }
+}
---
+++ КлассObjectИЕгоМетоды/Person2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace КлассObjectИЕгоМетоды
+{
+    internal class Person2
+    {
+        public string Name { get; set; } = "";
+        /// <summary>
+        /// Переопределение базоваго метода object
+        /// </summary>
+        /// <returns>
+        /// Возвращает инициализованное имя. Если его нет, выполняется базовый метод
+        /// </returns>
+        public override string? ToString()
+        {
+            if (string.IsNullOrEmpty(Name))
+                return base.ToString();
+            return Name;
+        }
+    }
+}
