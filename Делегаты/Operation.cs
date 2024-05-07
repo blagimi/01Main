177cff4cd6c7ccf0126bc415eb060e013d79e9a6
Alex <blagimi@outlook.com>
2024-05-08 02:30:16 +0500

    6.1.1

---
+++ Делегаты/Operation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Делегаты
+{
+    public delegate int MathOperation(int x, int y);
+    public class Operation
+    {     
+        public static int Add(int x, int y) => x + y;
+        public static int Multiply(int x, int y) => x * y;
+        public static int Substract(int x, int y) => x - y;
+        public static decimal Sqare(int x) => x * x;
+        public static int Double(int x) => x + x;
+    }
+}
