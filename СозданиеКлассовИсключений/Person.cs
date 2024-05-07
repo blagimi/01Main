d82ed152942b91c31689ec5dbe75f10323be9d03
Alex <blagimi@outlook.com>
2024-05-07 17:54:01 +0500

    5.5.1

---
+++ СозданиеКлассовИсключений/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace СозданиеКлассовИсключений
+{
+    internal class Person
+    {
+        private int age;
+        public string Name { get; set; } = "";
+        public int Age
+        {
+            get { return age; }
+            set
+            {
+                if (value < 18)
+                    throw new PersonException("Лицам до 18 регистрация запрещена",value);
+                else age = value;
+            }
+        }
+    }
+}
---
+++ СозданиеКлассовИсключений/PersonException.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace СозданиеКлассовИсключений
+{
+    /*
+     * Однако необязательно наследовать свой класс исключений именно от типа Exception, 
+     * можно взять какой-нибудь другой производный тип. Например, в данном случае мы
+     * можем взять тип ArgumentException, который представляет исключение, генерируемое в
+     * результате передачи аргументу метода некорректного значения
+     */
+    //internal class PersonException : Exception
+    internal class PersonException : ArgumentException
+    {
+        public int Value { get; }
+        /*
+         * По сути класс кроме пустого конструктора ничего не имеет, и то в конструкторе 
+         * мы просто обращаемся к конструктору базового класса Exception, 
+         * передавая в него строку message. Но теперь мы можем изменить класс Person,
+         * чтобы он выбрасывал исключение именно этого типа и соответственно в основной
+         * программе обрабатывать это исключение:
+         */
+        public PersonException(string message,int value) : base(message) { Value = value; }
+    }
+}
