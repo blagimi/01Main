9966e33d235a034b2a1c51edf26904f0c44a40d8
Alex <blagimi@outlook.com>
2024-05-18 00:44:43 +0500

    7.5.1

---
+++ НаследованиеИнтерфейсов/RunAction2.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace НаследованиеИнтерфейсов
+{
+    internal class RunAction2 : IRunAction
+    {
+        public void Move() => Console.WriteLine("I am tired");
+    }
+}
