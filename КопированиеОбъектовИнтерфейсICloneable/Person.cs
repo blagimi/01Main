6257cbc719c85d376ea3f3ec2f405e6d5820aa38
Alex <blagimi@outlook.com>
2024-05-20 23:56:31 +0500

    7.7.1

---
+++ КопированиеОбъектовИнтерфейсICloneable/Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace КопированиеОбъектовИнтерфейсICloneable
+{
+    internal class Person(string name, int age)
+    {
+        public string Name { get; set; } = name;
+        public int Age { get; set; } = age;
+    }
+}
