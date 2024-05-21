0b1c707b16a4fd851fc74f088b1905957c08d5fd
Alex <blagimi@outlook.com>
2024-05-21 23:53:03 +0500

    7.7.2

---
+++ КопированиеОбъектовИнтерфейсICloneable/Person2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace КопированиеОбъектовИнтерфейсICloneable
+{
+    internal class Person2
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public Person2(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+        public object Clone()
+        {
+            return new Person2(Name, Age);
+        }
+    }
+}
--- КопированиеОбъектовИнтерфейсICloneable/Program.cs
+++ КопированиеОбъектовИнтерфейсICloneable/Program.cs
@@ -13,3 +13,8 @@ var tom = new Person("Tom", 23);
 var bob = tom;
 bob.Name = "Bob";
 Console.WriteLine(tom.Name); // Bob
+
+var tom2 = new Person2("Tom", 23);
+var bob2 = (Person2)tom2.Clone();
+bob2.Name = "Bob";
+Console.WriteLine(tom2.Name); // Tom
