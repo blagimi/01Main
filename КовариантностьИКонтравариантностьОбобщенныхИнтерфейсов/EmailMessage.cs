cbcb5019ce9f56c540e47034088b4e768cd1e4d3
Alex <blagimi@outlook.com>
2024-05-23 23:36:06 +0500

    7.9.1

---
+++ КовариантностьИКонтравариантностьОбобщенныхИнтерфейсов/EmailMessage.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace КовариантностьИКонтравариантностьОбобщенныхИнтерфейсов
+{
+    internal class EmailMessage : Message
+    {
+        public EmailMessage(string text) : base(text) { }
+    }
+}
---
+++ КовариантностьИКонтравариантностьОбобщенныхИнтерфейсов/EmailMessenger.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace КовариантностьИКонтравариантностьОбобщенныхИнтерфейсов
+{
+    internal class EmailMessenger : IMessenger<EmailMessage>
+    {
+        public EmailMessage WriteMessage(string text)
+        {
+            return new EmailMessage($"Email: {text}");
+        }
+    }
+}
