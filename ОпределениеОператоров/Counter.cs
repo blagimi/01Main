10c17cc16941044563257443006bdf24f844028a
Alex <blagimi@outlook.com>
2024-05-24 23:43:45 +0500

    8.1.1

---
+++ ОпределениеОператоров/Counter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ОпределениеОператоров
+{
+    internal class Counter
+    {
+        public int Value { get; set; }
+
+        public static Counter operator +(Counter counter1, Counter counter2)
+        {
+            return new Counter { Value = counter1.Value + counter2.Value };
+        }
+        public static bool operator >(Counter counter1, Counter counter2)
+        {
+            return counter1.Value > counter2.Value;
+        }
+        public static bool operator <(Counter counter1, Counter counter2)
+        {
+            return counter1.Value < counter2.Value;
+        }
+    }
+}
---
+++ ОпределениеОператоров/Program.cs
@@ -0,0 +1,8 @@
+﻿using ОпределениеОператоров;
+Counter counter1 = new Counter { Value = 23 };
+Counter counter2 = new Counter { Value = 45 };
+bool result = counter1 > counter2;
+Console.WriteLine(result); // false
+
+Counter counter3 = counter1 + counter2;
+Console.WriteLine(counter3.Value);  // 23 + 45 = 68
