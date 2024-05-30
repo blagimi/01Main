33e3374cf3936b519ae7538976ff37b3232608fa
Alex <blagimi@outlook.com>
2024-05-31 03:30:00 +0500

    8.4.1

---
+++ Индексаторы/Matrix.cs
@@ -0,0 +1,11 @@
+﻿namespace Индексаторы;
+
+public class Matrix
+{
+    int[,] numbers = new int[,] {{1,2,4},{2,3,6},{3,4,8}};
+    public int this [int i, int j]
+    {
+        get => numbers [i,j];
+        set => numbers [i,j] = value;
+    }
+}
---
+++ Индексаторы/Person.cs
@@ -0,0 +1,9 @@
+﻿namespace Индексаторы;
+
+public class Person
+{
+    public string Name {get;} 
+    public Person (string name) => Name = name;
+
+
+}
