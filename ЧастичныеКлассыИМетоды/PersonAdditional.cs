4988b7d0ba22467753e31dda3462d833b36c69be
Alex <blagimi@outlook.com>
2024-05-31 23:03:34 +0500

    8.6.1

---
+++ ЧастичныеКлассыИМетоды/PersonAdditional.cs
@@ -0,0 +1,13 @@
+﻿namespace ЧастичныеКлассыИМетоды;
+
+public partial class Person
+{
+    partial void Read()
+    {
+        Console.WriteLine("I am reading book");
+    }
+    public void Eat()
+    {
+        Console.WriteLine("I am eat");
+    }
+}
---
+++ ЧастичныеКлассыИМетоды/PersonBase.cs
@@ -0,0 +1,15 @@
+﻿namespace ЧастичныеКлассыИМетоды;
+
+public partial class Person
+{
+    partial void Read();
+    public void ReadSomething()
+    {
+        Read();
+    }
+    public void Move()
+    {
+        Console.WriteLine("I am moving");
+        
+    }
+}
