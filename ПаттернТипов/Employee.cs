28f8a1c7ee5a8925a4268fa9200b0ed3548b983e
Alex <blagimi@outlook.com>
2024-06-18 23:55:42 +0500

    9.1.2

---
+++ ПаттернТипов/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace ПаттернТипов
+{
+    internal class Employee
+    {
+        public virtual void Work() => Console.WriteLine("Employee works");
+    }
+}
---
+++ ПаттернТипов/Manager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ПаттернТипов
+{
+    internal class Manager : Employee
+    {
+        public override void Work() => Console.WriteLine("Manager works");
+        public bool IsOnVacation { get; set; }
+    }
+}
