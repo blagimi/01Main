c89d0b8a4059ffb796724a0593fe25bb6c188f85
Alex <blagimi@outlook.com>
2024-06-25 23:58:18 +0500

    9.4.1

---
+++ ПозиционныйПаттерн/MessageDetails.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ПозиционныйПаттерн
+{
+    internal class MessageDetails
+    {
+        public string Language { get; set; } = "";
+        public string DateTime { get; set; } = "";
+        public string Status { get; set; } = "";
+
+        public void Deconstruct(out string lang, out string dateTime, out string status)
+        {
+            lang = Language;
+            dateTime = DateTime;
+            status = Status;
+        }
+    }
+}
---
+++ ПозиционныйПаттерн/Program.cs
@@ -0,0 +1,28 @@
+﻿using ПозиционныйПаттерн;
+
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+
+/*
+ * Позиционный паттерн применяется к типу у которого определен метод деконструктора.
+ * Используем позиционный паттерн и в зависимости от значений объекта MessageDetails
+ * возвратим определенное сообщение.
+ */
+
+MessageDetails details1 = new () { Language = "english", DateTime = "evening", Status = "user" };
+string message = GetWelcome(details1);
+Console.WriteLine(message);  // Good evening
+
+MessageDetails details2 = new () { Language = "french", DateTime = "morning", Status = "admin" };
+message = GetWelcome(details2);
+Console.WriteLine(message);  // Hello, Admin
+static string GetWelcome(MessageDetails details) => details switch
+{
+    ("english","morning",_) => "Good morning",
+    ("english","evening",_) => "Good evening",
+    ("german", "morning", _) => "Guten Morgen",
+    ("german", "evening", _) => "Guten Abend",
+    (_, _, "admin") => "Hello, Admin",
+    _ => "Здрасьте"
+};
+
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
