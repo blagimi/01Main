4fd718dac4ab00cba37b259bb54f9adf54c397f3
Alex <blagimi@outlook.com>
2024-07-19 23:42:58 +0500

    10.1.6

---
+++ СписокListT/Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace СписокListT
+{
+    internal class Person
+    {
+        public string Name { get; }
+        public Person(string name) => Name = name;
+    }
+}
