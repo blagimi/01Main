cc8933380346c10caf110c35e10a13eb37fca23b
Alex <blagimi@outlook.com>
2024-08-02 23:59:49 +0500

    10.1.14

--- СписокListT/Program.cs
+++ СписокListT/Program.cs
@@ -218,3 +218,27 @@ people13.RemoveRange(1, 2);
 // полностью очищаем список
 people13.Clear();
 // people = {  };
+
+
+/* Поиск и проверка элемента */
+
+var people14 = new List<string> () { "Eugene", "Mike", "Kate", "Tom", "Bob", "Sam" };
+ 
+var containsBob = people14.Contains("Bob");     //  true
+var containsBill = people14.Contains("Bill");    // false
+ 
+// проверяем, есть ли в списке строки с длиной 3 символа
+var existsLength3 = people14.Exists(p => p.Length == 3);  // true
+ 
+// проверяем, есть ли в списке строки с длиной 7 символов
+var existsLength7 = people14.Exists(p => p.Length == 7);  // false
+ 
+// получаем первый элемент с длиной в 3 символа
+var firstWithLength3 = people14.Find(p => p.Length == 3); // Tom
+ 
+// получаем последний элемент с длиной в 3 символа
+var lastWithLength3 = people14.FindLast(p => p.Length == 3);  // Sam
+ 
+// получаем все элементы с длиной в 3 символа в виде списка
+List<string> peopleWithLength3 = people14.FindAll(p => p.Length == 3);
+// peopleWithLength3 { "Tom", "Bob", "Sam"}
