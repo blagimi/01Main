cb3236c4a4e332adb0438cffff983f040db83d92
blagimi <blagimi@outlook.com>
2024-12-04 00:21:50 +0500

    10.9.9

--- КлассArrayИМассивы/Program.cs
+++ КлассArrayИМассивы/Program.cs
@@ -178,13 +178,13 @@ foreach (var person in people7)
 /* Копирование массива
 Метод Copy копирует часть одного массива в другой: */
 
-string[] people8 =  { "Tom", "Sam", "Bob", "Kate", "Tom", "Alice" };
+string[] people9 =  { "Tom", "Sam", "Bob", "Kate", "Tom", "Alice" };
  
 var employees = new string[3];
  
 // копируем 3 элемента из массива people c индекса 1  
 // и вставляем их в массив employees начиная с индекса 0
-Array.Copy(people8,1, employees,0, 3);
+Array.Copy(people9,1, employees,0, 3);
  
 foreach (var person in employees) 
     Console.Write($"{person} ");
@@ -193,8 +193,7 @@ foreach (var person in employees)
 /* В данном случае копируем 3 элемента из массива people начиная c индекса 1 и 
 вставляем их в массив employees начиная с индекса 0. */
 
-/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
-
 Console.ReadLine();
+
 
 /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
