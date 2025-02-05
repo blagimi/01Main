b114674dd4787035f8f65f88b4078a57f5535d14
Alex <blagimi@outlook.com>
2025-02-05 23:19:07 +0500

    12.1.2

--- HelloApp/Program.cs
+++ HelloApp/Program.cs
@@ -1,4 +1,7 @@
 ﻿
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.InteropServices;
+
 Console.WriteLine("Введите своё имя: ");
 string? name = Console.ReadLine();
 if(!string.IsNullOrEmpty(name))
