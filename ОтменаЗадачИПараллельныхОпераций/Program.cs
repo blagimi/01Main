59edb3d77c53dc37e9f0f38a26ae915ed82afff8
Alex <blagimi@outlook.com>
2025-04-24 12:42:43 +0500

    15.5.6

--- ОтменаЗадачИПараллельныхОпераций/Program.cs
+++ ОтменаЗадачИПараллельныхОпераций/Program.cs
@@ -409,6 +409,66 @@ ExampleFour();
 
 #endregion
 
+#region Передача токена во внешний метод
+
+/*
+
+Если операция, которая выполняется в задаче, представляет внешний метод, то ему можно передавать в качестве 
+одного из параметров:
+
+*/
+
+static void ExampleFive()
+{
+    CancellationTokenSource cancelTokenSource = new CancellationTokenSource();
+    CancellationToken token = cancelTokenSource.Token;
+    
+    Task task = new Task(() =>PrintSquares(token), token);
+    try
+    {
+        task.Start();
+        Thread.Sleep(1000);
+        // после задержки по времени отменяем выполнение задачи
+        cancelTokenSource.Cancel();
+    
+        // ожидаем завершения задачи
+        task.Wait();
+    }
+    catch (AggregateException ae)
+    {
+        foreach (Exception e in ae.InnerExceptions)
+        {
+            if (e is TaskCanceledException)
+                Console.WriteLine("Операция прервана");
+            else
+                Console.WriteLine(e.Message);
+        }
+    }
+    finally
+    {
+        cancelTokenSource.Dispose();
+    }
+    
+    //  проверяем статус задачи
+    Console.WriteLine($"Task Status: {task.Status}");
+    
+    
+    void PrintSquares(CancellationToken token)
+    {
+        for (int i = 1; i < 10; i++)
+        {
+            if (token.IsCancellationRequested)
+                token.ThrowIfCancellationRequested(); // генерируем исключение
+    
+            Console.WriteLine($"Квадрат числа {i} равен {i * i}");
+            Thread.Sleep(200);
+        }
+    }
+}
+
+ExampleFive();
+
+#endregion
 /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
 
 Console.ReadLine();
