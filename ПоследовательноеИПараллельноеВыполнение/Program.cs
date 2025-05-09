cc07dd2dde7392b7a4a25361a7d8a794734e4759
Alex <blagimi@outlook.com>
2025-05-09 23:59:31 +0500

    16.3.2

--- ПоследовательноеИПараллельноеВыполнение/Program.cs
+++ ПоследовательноеИПараллельноеВыполнение/Program.cs
@@ -29,7 +29,7 @@ Example();
 
 /*
 
-онсольный вывод данной программы:
+консольный вывод данной программы:
 
 Hello C#
 Hello World
@@ -109,7 +109,6 @@ Example3();
 одна задача. Но для ожидания выполнения к Task.WhenAny() применяется оператор await:
 
 */
-
 async static void Example4()
 {
     // определяем и запускаем задачи
@@ -127,7 +126,66 @@ async static void Example4()
     }
 }
 
-Example4();
+Example4(); 
+
+#region Получение результата
+
+/*
+
+Задачи, передаваемые в Task.WhenAll и Task.WhenAny, могут возвращать некоторое значение. В этом случае из методов Task.WhenAll и Task.WhenAny можно получить массив, который будет содержать результаты задач:
+
+*/
+
+async static void Example5()
+{
+    // определяем и запускаем задачи
+    var task1 = SquareAsync(4);
+    var task2 = SquareAsync(5);
+    var task3 = SquareAsync(6);
+    
+    // ожидаем завершения всех задач
+    int[] results = await Task.WhenAll(task1, task2, task3);
+    // получаем результаты:
+    foreach (int result in results)
+        Console.WriteLine(result);
+    
+    async Task<int> SquareAsync(int n)
+    {
+        await Task.Delay(1000);
+        return n * n;
+    }
+}
+
+Example5();
+
+/*
+В данном случае метод Square возвращает число int - квадрат передаваемого в метод числа. И переменная results будет содержать результат вызова Task.WhenAll - по сути результаты всех трех запущенных задач. Поскольку все передаваемые в Task.WhenAll задачи возвращают int, то соответственно результат Task.WhenAll будет представлять массив значений int.
+
+Также после завершения задачи ее результат можно получить стандартным образом через свойство Result:
+*/
+
+async static void Example6()
+{
+    // определяем и запускаем задачи
+    var task1 = SquareAsync(4);
+    var task2 = SquareAsync(5);
+    var task3 = SquareAsync(6);
+    
+    await Task.WhenAll(task1, task2, task3);
+    // получаем результат задачи task2
+    Console.WriteLine($"task2 result: {task2.Result}"); // task2 result: 25
+    
+    async Task<int> SquareAsync(int n)
+    {
+        await Task.Delay(1000);
+        return n * n;
+    }
+}
+
+Example6();
+
+#endregion
+
 
 /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
 
