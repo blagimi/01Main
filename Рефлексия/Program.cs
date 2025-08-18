212fa9ba10ad3979d076451dab7b8a5dad83f101
Alex <blagimi@outlook.com>
2025-08-18 18:35:42 +0500

    19.1.2

--- Рефлексия/Program.cs
+++ Рефлексия/Program.cs
@@ -74,8 +74,129 @@ ParameterInfo: класс, хранящий информацию о параме
 
 #endregion
 
+#region Получение типа
+
+/*
+
+Чтобы управлять типом и получать всю информацию о нем, нам надо сперва получить данный тип. Это можно сделать 
+тремя способами: с помощью оператора typeof, с помощью метода GetType() класса Object и применяя статический 
+метод Type.GetType().
+
+Получение типа через typeof:
+
+*/
+Type myType = typeof(Person);
+
+Console.WriteLine(myType);  // Person
+
+/*
+public class Person
+{
+    public string Name { get; }
+    public Person(string name) => Name = name;
+}
+
+Здесь определен класс Person с некоторой функциональностью. И чтобы получить его тип, используется выражение 
+Type myType = typeof(Person);
+
+Получение типа с помощью метода GetType, унаследованного от класса Object:
+
+*/
+
+Person tom = new Person("Tom");
+
+Type myType2 = tom.GetType();
+
+
+/*
+В отличие от предыдущего примера здесь, чтобы получить тип, надо создавать объект класса.
+
+И третий способ получения типа - статический метод Type.GetType():
+*/
+
+Type? myType3 = Type.GetType("Person", false, true);
+
+/*
+Первый параметр указывает на полное имя класса с пространством имен. Второй параметр указывает, будет ли 
+генерироваться исключение, если класс не удастся найти. В данном случае значение false означает, что исключение 
+не будет генерироваться. И третий параметр указывает, надо ли учитывать регистр символов в первом параметре. 
+Значение true означает, что регистр игнорируется. Поскольку указанный тип может отсутствовать, то метод возвращает 
+объект nullable-типа
+
+В данном случае класс основной программы и класс Person находятся в глобальном пространстве имен. Однако если 
+тип располагается в другом пространстве имен, то его также надо указать:
+*/
+
+Type? myType4 = Type.GetType("PeopleTypes.Person", false, true);
+ 
+Console.WriteLine(myType4);  // PeopleTypes.Person
+
+/* 
+namespace PeopleTypes
+{
+    public class Person
+    {
+        public string Name { get; }
+        public Person(string name) => Name = name;
+    }
+}
+В качестве альтернативы можно применять оператор typeof, передавая в него имя типа с указанием пространства имен:
+*/
+
+Type myType5 = typeof(PeopleTypes.Person);
+
+/*
+Если нужный нам тип находится в другой сборке dll, то после полного имени класса через запятую указывается имя сборки:
+*/
+
+Type? myType6 = Type.GetType("PeopleTypes.Person, MyLibrary", false, true);
+
+/*
+Теперь исследуем тип и получим некоторую информацию о нем.
+*/
+
+
+Type myType7 = typeof(PeopleTypes.Person);
+ 
+Console.WriteLine($"Name: {myType7.Name}");              // получаем краткое имя типа
+Console.WriteLine($"Full Name: {myType7.FullName}");     // получаем полное имя типа
+Console.WriteLine($"Namespace: {myType7.Namespace}");    // получаем пространство имен типа
+Console.WriteLine($"Is struct: {myType7.IsValueType}");  // является ли тип структурой
+Console.WriteLine($"Is class: {myType7.IsClass}");       // является ли тип классом
+ 
+/*
+Консольный вывод:
+
+Name: Person
+Full Name: PeopleTypes.Person
+Namespace: PeopleTypes
+Is struct: False
+Is class: True
+
+*/
+
+
+#endregion
+
+
 /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
 
 Console.ReadLine();
 
 /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+
+
+public class Person
+{
+    public string Name { get; }
+    public Person(string name) => Name = name;
+}
+
+namespace PeopleTypes
+{
+    public class Person
+    {
+        public string Name { get;}
+        public Person(string name) => Name = name;
+    }
+}
