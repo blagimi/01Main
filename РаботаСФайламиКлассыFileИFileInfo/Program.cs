5290d8abcdfeb866300d1163f5a25f107a00f665
Alex <blagimi@outlook.com>
2026-05-05 23:43:46 +0500

    22.3.5

--- РаботаСФайламиКлассыFileИFileInfo/Program.cs
+++ РаботаСФайламиКлассыFileИFileInfo/Program.cs
@@ -181,4 +181,102 @@ TCopy();
 */
 
 #endregion
+
+#region Чтение и запись файлов
+
+/*
+В дополнение к вышерассмотренным методам класс File также предоставляет ряд методов для 
+чтения-записи текстовых и бинарных файлов:
+
+AppendAllLines(String, IEnumerable<String>) / AppendAllLinesAsync(String, IEnumerable<String>, 
+CancellationToken)
+
+добавляют в файл набор строк. Если файл не существует, то он создается
+
+AppendAllText(String, String) / AppendAllTextAsync(String, String, CancellationToken)
+
+добавляют в файл строку. Если файл не существует, то он создается
+
+byte[] ReadAllBytes (string path) / Task<byte[]> ReadAllBytesAsync (string path, 
+CancellationToken cancellationToken)
+
+считывают содержимое бинарного файла в массив байтов
+
+string[] ReadAllLines (string path) / Task<string[]> ReadAllLinesAsync (string path, 
+CancellationToken cancellationToken)
+
+считывают содержимое текстового файла в массив строк
+
+string ReadAllText (string path) / Task<string> ReadAllTextAsync (string path, 
+CancellationToken cancellationToken)
+
+считывают содержимое текстового файла в строку
+
+IEnumerable<string> ReadLines (string path)
+
+считывают содержимое текстового файла в коллекцию строк
+
+void WriteAllBytes (string path, byte[] bytes) / Task WriteAllBytesAsync 
+(string path, byte[] bytes, CancellationToken cancellationToken)
+
+записывают массив байт в бинарный файл. Если файл не существует, он создается. Если существует, 
+то перезаписывается
+
+void WriteAllLines (string path, string[] contents) / Task WriteAllLinesAsync (string path, 
+IEnumerable<string> contents, CancellationToken cancellationToken)
+
+записывают массив строк в текстовый файл. Если файл не существует, он создается. Если существует, 
+то перезаписывается
+
+WriteAllText (string path, string? contents) / Task WriteAllTextAsync (string path, 
+string? contents, CancellationToken cancellationToken)
+
+записывают строку в текстовый файл. Если файл не существует, он создается. Если существует, то 
+перезаписывается
+
+Как видно, эти методы покрывают практически все основные сценарии - чтение и запись текстовых и 
+бинарных файлов. Причем в зависимости от задачи можно применять как синхронные методы, так и их 
+асинхронные аналоги.
+
+Например, запишем и считаем обратно в строку текстовый файл:
+*/
+
+static async void TRead()
+{
+    string path = @"c:\Test\content.txt";
+ 
+string originalText = "Hello";
+// запись строки
+await File.WriteAllTextAsync(path, originalText);
+// дозапись в конец файла
+await File.AppendAllTextAsync(path, "\nHello work");
+ 
+// чтение файла
+string fileText = await File.ReadAllTextAsync(path);
+Console.WriteLine(fileText);
+}
+
+TRead();
+
+/*
+Консольный вывод:
+
+Hello 
+Hello work
+Стоит отметить, что при добавлении текста я добавил в строку последовательность "\n", 
+которая выполняет перевод на следующую строку. Благодаря этому добавляемый текст располагается 
+в файле на новой строке.
+
+Если мы хотим, что в файле изначально шло добавление на новую строку, то для записи стоит 
+использовать метод WriteAllLines/ WriteAllLinesAsync, а для добавления - AppendAllLines 
+/ AppendAllLinesAsync
+
+1
+await File.WriteAllLinesAsync(path, new[] { "Hello", "Hello work" });
+Аналогично при чтении файла если мы хотим каждую строку файла считать отдельно, то вместо 
+ReadAllText / ReadAllTextAsync применяется ReadAllLines / ReadAllLinesAsync.
+*/
+
+#endregion
+
 Console.ReadLine();
