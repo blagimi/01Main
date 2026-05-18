7b44df0db8a2b7e193d63f24cfcfc97753c5865f
Alex <blagimi@outlook.com>
2026-05-18 22:45:41 +0500

    22.6.2

--- БинарныеФайлы/Program.cs
+++ БинарныеФайлы/Program.cs
@@ -1,1 +1,133 @@
-﻿Console.WriteLine("Hello, World!");
+﻿#region Бинарные файлы. BinaryWriter и BinaryReader
+
+/*
+ * Для работы с бинарными файлами предназначена пара классов BinaryWriter и BinaryReader. 
+ * Эти классы позволяют читать и записывать данные в двоичном формате. 
+*/
+
+#endregion
+
+#region BinaryWriter
+
+/*
+Для создания объекта BinaryWriter можно применять ряд конструкторов. Возьмем наиболее простую:
+
+BinaryWriter(Stream stream)
+в его конструктор передается объект Stream (обычно это объект FileStream).
+
+Основные методы класса BinaryWriter
+
+Close(): закрывает поток и освобождает ресурсы
+
+Flush(): очищает буфер, дописывая из него оставшиеся данные в файл
+
+Seek(): устанавливает позицию в потоке
+
+Write(): записывает данные в поток. В качестве параметра этот метод может принимать значения примитивных данных:
+
+Write(bool)
+
+Write(byte)
+
+Write(char)
+
+Write(decimal)
+
+Write(double)
+
+Write(Half)
+
+Write(short)
+
+Write(int)
+
+Write(long)
+
+Write(sbyte)
+
+Write(float)
+
+Write(string)
+
+Write(ushort)
+
+Write(uint)
+
+Write(ulong)
+
+Либо можно передать массивы типов byte и char
+
+Write(byte[])
+
+Write(char[])
+
+Write(ReadOnlySpan<byte>)
+
+Write(ReadOnlySpan<char>)
+
+При записи массива дополнительно можно указать, с кого элемента массива надо выполнять запись, а также число записываемых элементов массива:
+
+Write(byte[], int, int)
+
+Write(char[], int, int)
+
+Рассмотрим простейшую запись бинарного файла:
+*/
+
+string path = "person.dat";
+
+// создаем объект BinaryWriter
+using (BinaryWriter writer = new BinaryWriter(File.Open(path, FileMode.OpenOrCreate)))
+{
+    // записываем в файл строку
+    writer.Write("Tom");
+    // записываем в файл число int
+    writer.Write(37);
+    Console.WriteLine("File has been written");
+}
+
+/*
+ * Здесь в файл person.dat записываются два значения: строка "Tom" и число 37. Для создание объекта применяется вызов new BinaryWriter(File.Open(path, FileMode.OpenOrCreate))
+
+    Подобным образом можно сохранять более сложные данные. Например, сохраним в файл массив объектов:
+ */
+
+string path2 = "people.dat";
+
+// массив для записи
+Person[] people =
+{
+    new Person("Tom", 37),
+    new Person("Bob", 41)
+};
+
+using (BinaryWriter writer = new BinaryWriter(File.Open(path2, FileMode.OpenOrCreate)))
+{
+    // записываем в файл значение каждого свойства объекта
+    foreach (Person person in people)
+    {
+        writer.Write(person.Name);
+        writer.Write(person.Age);
+    }
+    Console.WriteLine("File has been written");
+}
+
+/*
+ * В данном случае последовательно сохраняем в файл people.dat данные объектов Person из массива people.
+ */
+
+
+#endregion
+
+Console.ReadLine();
+
+class Person
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public Person(string name, int age)
+    {
+        Name = name;
+        Age = age;
+    }
+}
