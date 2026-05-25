525049ab99f5b6a9b2ee7861784efcca2b1b7a9b
Alex <blagimi@outlook.com>
2026-05-25 18:57:17 +0500

    23.1.1

---
+++ JSON/Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JSON
+{
+    internal class Person
+    {
+        byte age = 0;
+        public string Name { get; }
+        public byte Age
+        {
+            set
+            {
+                if (value < 0 || value > 120)
+                    Console.WriteLine("Ошибка обработки возраста. \nВозраст должен быть в диапазоне от 0 до 120");
+                else
+                    age = value;
+            }
+            get { return age; }
+        }
+        public Person (string name, byte age)
+        {
+            Name = name;
+            Age = age;
+        }
+    }
+}
---
+++ JSON/Program.cs
@@ -0,0 +1,62 @@
+﻿using JSON;
+using System.Text.Json;
+
+#region Сериализация в JSON. JsonSerializer
+
+/*
+ * JSON (JavaScript Object Notation) является одним из наиболее популярных форматов для хранения и передачи данных. 
+ * И платформа .NET предоставляет функционал для работы с JSON.
+
+Основная функциональность по работе с JSON сосредоточена в пространстве имен System.Text.Json. Ключевым типом является класс JsonSerializer, который и позволяет 
+сериализовать объект в json и, наоборот, десериализовать код json в объект C#.
+
+Для сохранения объекта в json в классе JsonSerializer определен статический метод Serialize() и его асинхронный двойник SerializeAsyc(), которые имеют ряд 
+перегруженных версий. Некоторые из них:
+
+string Serialize(Object obj, Type type, JsonSerializerOptions options): сериализует объект obj типа type и возвращает код json в виде строки. Последний 
+необязательный параметр options позволяет задать дополнительные опции сериализации
+
+string Serialize<T>(T obj, JsonSerializerOptions options): типизированная версия сериализует объект obj типа T и возвращает код json в виде строки.
+
+Task SerializeAsync(Stream utf8Json, Object obj, Type type, JsonSerializerOptions options): сериализует объект obj типа type и записывает его в поток utf8Json. 
+Последний необязательный параметр options позволяет задать дополнительные опции сериализации
+
+Task SerializeAsync<T>(Stream utf8Json, T obj, JsonSerializerOptions options): типизированная версия сериализует объект obj типа T в поток utf8Json.
+
+Для десериализации кода json в объект C# применяется метод Deserialize() и его асинхронный двойник DeserializeAsync(), которые имеют различные версии. 
+Некоторые из них:
+
+object? Deserialize(string json, Type type, JsonSerializerOptions options): десериализует строку json в объект типа type и возвращает десериализованный объект. 
+Последний необязательный параметр options позволяет задать дополнительные опции десериализации
+
+T? Deserialize<T>(string json, JsonSerializerOptions options): десериализует строку json в объект типа T и возвращает его.
+
+ValueTask<object?> DeserializeAsync(Stream utf8Json, Type type, JsonSerializerOptions options, CancellationToken token): десериализует данные из 
+потока utf8Json, который представляет объект JSON, в объект типа type. Последние два параметра необязательны: options позволяет задать дополнительные 
+опции десериализации, а token устанавливает CancellationToken для отмены задачи. Возвращается десериализованный объект, обернутый в ValueTask
+
+ValueTask<T?> DeserializeAsync<T>(Stream utf8Json, JsonSerializerOptions options, CancellationToken token): десериализует данные из потока utf8Json, 
+который представляет объект JSON, в объект типа T. Возвращается десериализованный объект, обернутый в ValueTask
+
+Рассмотрим применение класса на простом примере. Сериализуем и десериализуем простейший объект:
+ */
+
+
+Person tom = new Person("Tom", 37);
+string json = JsonSerializer.Serialize(tom);
+Console.WriteLine(json);
+Person? restoredPerson = JsonSerializer.Deserialize<Person>(json);
+Console.WriteLine(restoredPerson?.Name); // Tom
+
+/*
+ * Здесь вначале сериализуем с помощью метода JsonSerializer.Serialize() объект типа Person в стоку с кодом json. 
+ * Затем обратно получаем из этой строки объект Person посредством метода JsonSerializer.Deserialize().
+
+Консольный вывод:
+
+{"Name":"Tom","Age": 37}
+Tom
+Хотя в примере выше сериализовался/десериализовался объект класса, но подобным способом мы также можем сериализовать/десериализовать структуры.
+ */
+
+#endregion
