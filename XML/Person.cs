5227b23a5a01a839df19b805c1be125d2c5b429c
Alex <blagimi@outlook.com>
2026-06-01 23:29:41 +0500

    24.1.1

---
+++ XML/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XML
+{
+    internal class Person
+    {
+        public string Name { get;}
+        public int Age { get; set; }
+        public string Company { get; set; }
+        public Person(string name, int age, string company)
+        {
+            Name = name;
+            Age = age;
+            Company = company;
+        }
+    }
+}
