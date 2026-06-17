03ad423e6c7dbecad394887143708043aad56fa2
Alex <blagimi@outlook.com>
2026-06-17 23:59:57 +0500

    24.7.2

--- ИзменениеДокументаВLINQtoXML/Program.cs
+++ ИзменениеДокументаВLINQtoXML/Program.cs
@@ -64,3 +64,53 @@ Console.WriteLine(xdoc);
  */
 
 #endregion
+
+
+#region Изменение данных
+
+//Для изменения данных в документ xml необходимо получить элемент, который надо изменить, и затем можно отредактировать значения его отдельных атрибутов или вложенных элементов. Изменим элемент person, в котором атрибут name = "Tom":
+
+ 
+XDocument xdoc = XDocument.Load("people.xml");
+
+// получим элемент person с name = "Tom"
+var tom = xdoc.Element("people")?
+    .Elements("person")
+    .FirstOrDefault(p => p.Attribute("name")?.Value == "Tom");
+
+if (tom != null)
+{
+    //  меняем атрибут name
+    var name = tom.Attribute("name");
+    if (name != null) name.Value = "Tomas";
+
+
+    //  меняем вложенный элемент age
+    var age = tom.Element("age");
+    if (age != null) age.Value = "22";
+
+    xdoc.Save("people.xml");
+}
+
+// выводим xml-документ на консоль
+Console.WriteLine(xdoc);
+/*
+В результате сформируется и сохранится на диск новый документ:
+
+< people >
+  < person name = "Tomas" >
+    < company > Microsoft </ company >
+    < age > 22 </ age >
+  </ person >
+  < person name = "Bob" >
+    < company > Google </ company >
+    < age > 41 </ age >
+  </ person >
+  < person name = "Sam" >
+    < company > JetBrains </ company >
+    < age > 28 </ age >
+  </ person >
+</ people >
+*/
+
+#end region
